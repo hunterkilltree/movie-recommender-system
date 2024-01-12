246779f4f9518d399b7fa4fbae6e11cb7539d9c7
hunterkilltree <snguoila@yahoo.com>
2024-01-12 07:59:37 +0700

    update

--- src/test/java/io/datajek/spring/basics/movierecommendersystem/StudentServiceTest.java
+++ src/test/java/io/datajek/spring/basics/movierecommendersystem/StudentServiceTest.java
@@ -8,23 +8,23 @@ import static org.mockito.Mockito.when;
 
 class StudentServiceMockTest {
 
-  @Test
-  void testFindTotal() {
-    StudentDao studentDaoMock = mock(StudentDao.class);
-    when(studentDaoMock.getMarks()).thenReturn(new int[] {15, 20, 5});
-    StudentService studentService = new StudentService(studentDaoMock);
-    int total = studentService.findTotal();
-    assertEquals(40, total);
-  }
-
-  @Test
-  void testFindTotal_EmptyArray() {
-    StudentDao studentDaoMock = mock(StudentDao.class);
-    when(studentDaoMock.getMarks()).thenReturn(new int[] {});
-    StudentService studentService = new StudentService(studentDaoMock);
-    int total = studentService.findTotal();
-    assertEquals(0, total);
-  }
+//  @Test
+//  void testFindTotal() {
+//    StudentDao studentDaoMock = mock(StudentDao.class);
+//    when(studentDaoMock.getMarks()).thenReturn(new int[] {15, 20, 5});
+//    StudentService studentService = new StudentService(studentDaoMock);
+//    int total = studentService.findTotal();
+//    assertEquals(40, total);
+//  }
+//
+//  @Test
+//  void testFindTotal_EmptyArray() {
+//    StudentDao studentDaoMock = mock(StudentDao.class);
+//    when(studentDaoMock.getMarks()).thenReturn(new int[] {});
+//    StudentService studentService = new StudentService(studentDaoMock);
+//    int total = studentService.findTotal();
+//    assertEquals(0, total);
+//  }
 }
 
 
